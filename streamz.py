c6919832e6a8138ceafed7d919b696105cc3a241
arnenupharsah <arnenupharsah@gmail.com>
2019-02-27 19:51:04 +0530

    Added Register page

--- streamz.py
+++ streamz.py
@@ -8,6 +8,7 @@ from web import form
 urls = (
 	'/', 'Index',
 	'/home', 'Homepage',
+	'/register', 'Register',
  
 )
 
@@ -19,28 +20,26 @@ render = web.template.render('templates', base='base')
 class Index:
 
 	login = form.Form(
-	form.Textbox('loginemail'),
-	form.Password('loginpassword'),
-	form.Button('loginbtn'),
-	)
-
-	register = form.Form(
-	form.Textbox('firstname'),
-	form.Textbox('lastname'),
-	form.Textbox('phoneno'),
 	form.Textbox('email'),
-	form.Password('registerpassword'),
-	form.Button('registerbtn'),
+	form.Password('password'),
+	form.Button('Login'),
 	)
 
 	def GET(self):
 		""" Show page """
 		
 		login = self.login()
-		register = self.register()
-		return render.index(login, register)
+		return render.index(login)
+
+
 
 	def POST(self):
+		login = self.login()
+		if not login.validates():
+			return "Unsuccesful Login"
+		else:
+			return login.d.email,login.d.password
+
 		""" Add new entry 
 		form = self.form()
 		if not form.validates():
@@ -48,8 +47,8 @@ class Index:
 		    return render.index(todos, form)
 		model.new_todo(form.d.title)
 		raise web.seeother('/')
-
-		"""
+"""
+		
 
 class Homepage:
 
@@ -57,6 +56,30 @@ class Homepage:
 		""" Show page """
 		
 		return render.homepage()
+
+class Register:
+	register = form.Form(
+	form.Textbox('firstname'),
+	form.Textbox('lastname'),
+	form.Textbox('phone'),
+	form.Textbox('email'),
+	form.Password('password'),
+	form.Button('Register'),
+	)
+
+	def GET(self):
+		""" Show page """
+		register = self.register()
+		return render.register(register)
+
+	def POST(self):
+		register = self.register()
+		if not register.validates():
+			return "Unsuccesful Registration"
+		else:
+			return register.d.firstname,register.d.lastname,register.d.phone,register.d.email,register.d.password
+
+
 
 app = web.application(urls, globals())
 
