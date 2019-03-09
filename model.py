370328effa0d89ce48e32e5a11c943f931efa11c
arnenupharsah <arnenupharsah@gmail.com>
2019-03-09 15:19:49 +0530

    Publish Video Page

--- model.py
+++ model.py
@@ -7,16 +7,32 @@ url1 = 'http://0.0.0.0:8080/getuser'
 
 def new_user(firstname,lastname,phone,email,password):
     params = {'firstname': firstname,'lastname':lastname,'phone':phone,'email':email,'password':password} 
-    requests.post(url1, data=json.dumps(params))
-    #return json.dumps(params)
+    #requests.post(url1, data=json.dumps(params))
+    return json.dumps(params)
 
 def check_user(username,password):
     params = {'username': username,'password':password} 
-    requests.post(url1, data=json.dumps(params))
-    #return json.dumps(params)
+    #requests.post(url1, data=json.dumps(params))
+    return json.dumps(params)
 
 def send_search(search_text):
     params = {'search_text':search_text} 
-    requests.post(url1, data=json.dumps(params))
-    #return json.dumps(params)
+    #requests.post(url1, data=json.dumps(params))
+    return json.dumps(params)
+
+def send_comment(search_text):
+    params = {'comment_text':search_text} 
+    #requests.post(url1, data=json.dumps(params))
+    return json.dumps(params)
+
+def upload_video(name):
+    params = {'name':name} 
+    #requests.post(url1, data=json.dumps(params))
+    return json.dumps(params)
+
+def search_upload_video(path):
+    params = {'name':name,'description':description,'tags':tags} 
+    #requests.post(url1, data=json.dumps(params))
+    return json.dumps(params)
+    
     
--- streamz.py
+++ streamz.py
@@ -14,14 +14,59 @@ urls = (
 	'/uploads', 'Uploads',
 	'/statistics', 'Statistics',
 	'/uploadvideo','Uploadvideo',
-	'/getuser','Getuser'
+	'/uploaded','Uploaded',
+	'/comment','Comment',
+	'/searchresults','SearchResults',
+	'/uploadvideodesc','UploadVideoDesc'
+
  
 )
 
 
 ### Templates
 render = web.template.render('templates', base='base')
+render1 = web.template.render('templates', base='topnav')
 
+class UploadVideoDesc:
+
+	def GET(self):
+		return render.uploadvideodesc()
+
+	def POST(self):
+		x = web.input()
+		
+
+		
+
+class Uploaded:
+
+	def POST(self):
+		x = web.input(myfile={})
+		filedir = 'static/video' # change this to the directory you want to store the file in.
+		if 'myfile' in x: # to check if the file-object is created
+			filepath=x.myfile.filename.replace('\\','/') # replaces the windows-style slashes with linux ones.
+			filename=filepath.split('/')[-1] # splits the and chooses the last part (the filename with extension)
+			fout = open(filedir +'/'+ filename,'w') # creates the file where the uploaded file should be stored
+			fout.write(x.myfile.file.read()) # writes the uploaded file to the newly created file.
+			fout.close() # closes the file, upload complete.
+			s = model.upload_video(filename)
+			return s
+
+class SearchResults:
+
+
+	def POST(self):
+		i = web.input()
+		s = model.send_search(i.searchtext)
+		return s
+
+class Comment:
+
+
+	def POST(self):
+		i = web.input()
+		s = model.send_comment(i.commenttext)
+		return s		
 
 class Index:
 
@@ -51,36 +96,22 @@ class Index:
 
 		
 
+
+
 class Homepage:
 
 
 
 	def GET(self):
-		""" Show page """
-		
+		""" Show page """		
 		return render.homepage()
 
 class Video:
 
-	searchform = form.Form(
-	form.Textbox('searchtext'),
-	form.Button('Search'),
-	)
 
 	def GET(self):
 		""" Show page """
-		searchform = self.searchform()
-		return render.video(searchform)
-
-	def POST(self):
-		searchform = self.searchform()
-		if not searchform.validates():            
-			return "Unsuccessful"
-		txt=searchform.d.searchtext
-		s = model.send_search(txt)
-		return s
-	
-
+		return render.video()
 
 class About:
 
@@ -106,8 +137,7 @@ class Statistics:
 class Uploadvideo:
 
 	def GET(self):
-		""" Show page """
-		
+		""" Show page """		
 		return render.uploadvideo()
 
 class Register:
@@ -137,13 +167,6 @@ class Register:
 		pwd=register.d.password
 		s = model.new_user(fn,ln,ph,eml,pwd)
 		return s
-
-class Getuser:
-
-	def POST(self):
-		""" Show page """
-		user = web.data()
-		return user
 
 
 app = web.application(urls, globals())
